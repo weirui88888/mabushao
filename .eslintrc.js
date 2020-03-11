d73266c9ab8f335c1a07194136402d6ac86080ac
魏瑞 <weirui@weiruideMBP.lan>
2020-03-11 14:32:06 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,32 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
     // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "vue/no-unused-components": ["error", {
-     "ignoreWhenBindingPresent": true
-   }],
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/no-unused-components": [
+      "error",
+      {
+        ignoreWhenBindingPresent: true
+      }
+    ],
+    "space-before-function-paren": 0,
+    semi: 0,
+    quotes: 0
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         jest: true
       }
     }
   ]
-}
+};
